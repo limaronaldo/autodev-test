fd7fca8597635d8e18d5018d461a333067160731
Ronaldo Martins <ron@ldinho.com.br>
2025-12-12 17:28:58 -0300

    feat: add cube function that returns n³

--- src/cube.ts
+++ src/cube.ts
@@ -1,8 +1,3 @@
-/**
- * Calculates the cube of a number.
- * @param n - The number to cube
- * @returns The cube of n (n * n * n)
- */
 export default function cube(n: number): number {
   return n * n * n;
 }
