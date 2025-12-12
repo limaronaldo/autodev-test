29cefcb06103edd9388dafe394625efb72c7d004
Ronaldo Martins <ron@ldinho.com.br>
2025-12-12 18:46:48 -0300

    feat: add iterative fibonacci math utility

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export function modulo(a: number, b: number): number {
   if (b === 0) throw new Error("Cannot take modulo by zero");
   return a % b;
 }
+
+export { subtract, fibonacci } from "./math";
