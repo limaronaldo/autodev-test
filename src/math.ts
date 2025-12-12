0b09cce6638a1a049281c70eb7045e9d92ea7386
Ronaldo Martins <ron@ldinho.com.br>
2025-12-12 18:07:36 -0300

    feat: add multiply utility

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,10 @@
+
+/**
+ * Returns the arithmetic product of two numbers.
+ */
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
 export function multiply(a: number, b: number): number {
   return a * b;
 }
