d1e55ca0e660beccef7246c7824c059a298b80d7
Ronaldo Martins <ron@ldinho.com.br>
2025-12-12 18:39:26 -0300

    feat: add modulo utility function (#55)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export function hello() { return "Hello, World!"; }
+
+export function modulo(a: number, b: number): number {
+  if (b === 0) throw new Error("Cannot take modulo by zero");
+  return a % b;
+}
