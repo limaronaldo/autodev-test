84aa99abd0dc62f017ab854f8e9a54012bdcac97
Ronaldo Martins <ron@ldinho.com.br>
2025-12-15 13:59:18 -0300

    feat: add abs function to math module with tests

--- src/math.ts
+++ src/math.ts
@@ -5,3 +5,11 @@
 export function subtract(a: number, b: number): number {
   return a - b;
 }
+
+/**
+ * Get the absolute value of a number.
+ */
+export function abs(x: number): number {
+  return Math.abs(x);
+}
+
