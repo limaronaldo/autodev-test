27fbd980d861069ee01c2340e9a6a5f4fee165fe
AlexeiLuchian <alexeiluchian999@gmail.com>
2025-12-15 23:58:13 +0200

    feat: add max function

--- src/math.ts
+++ src/math.ts
@@ -12,3 +12,10 @@ export function subtract(a: number, b: number): number {
 export function sqrt(x: number): number {
   return Math.sqrt(x);
 }
+
+/**
+ * Get the maximum of two numbers.
+ */
+export function max(a: number, b: number): number {
+  return Math.max(a, b);
+}
