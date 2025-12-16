adb85bda9db074328dacefd7c4be399fb8a02337
Ronaldo Martins <ron@ldinho.com.br>
2025-12-16 17:09:03 -0300

    feat: add abs, square, cube math functions
    Fixes #78, #76, #75, #74

--- src/math.ts
+++ src/math.ts
@@ -1,4 +1,3 @@
-
 /**
  * Subtract two numbers.
  */
@@ -6,8 +5,25 @@ export function subtract(a: number, b: number): number {
   return a - b;
 }
 
-export function max(a: number, b: number): number {
-  return Math.max(a, b);
+/**
+ * Get the absolute value of a number.
+ */
+export function abs(x: number): number {
+  return Math.abs(x);
+}
+
+/**
+ * Square a number.
+ */
+export function square(x: number): number {
+  return x * x;
+}
+
+/**
+ * Calculate the cube of a number.
+ */
+export function cube(x: number): number {
+  return x * x * x;
 }
 
 /**
@@ -15,4 +31,11 @@ export function max(a: number, b: number): number {
  */
 export function sqrt(x: number): number {
   return Math.sqrt(x);
+}
+
+/**
+ * Get the maximum of two numbers.
+ */
+export function max(a: number, b: number): number {
+  return Math.max(a, b);
 }
